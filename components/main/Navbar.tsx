7565d8472d3a6e6d81555255e909d53bb302757c
fatihcaliss <fthcls@gmail.com>
2024-01-09 01:14:07 +0300

    navbar try

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20 max-sm:hidden">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
@@ -38,7 +38,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row gap-1 sm:gap-5">
           {Socials.map((social) => (
             <Link key={social.name} href={social?.url}>
               <Image
