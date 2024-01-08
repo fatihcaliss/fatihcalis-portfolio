c5bb1ec7a371792a016521c0b5a4971eaee43875
fatihcaliss <fthcls@gmail.com>
2024-01-09 00:30:50 +0300

    update assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,29 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Fatih Calis",
-  description: "Fatih Calis Portolio Wbsite",
+  description: "Fatih Calis Portolio Website",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/images/darkfavicon.png",
+        href: "/images/darkfavicon.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/images/favicon.png",
+        href: "/images/favicon.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +36,10 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <StarsCanvas />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
