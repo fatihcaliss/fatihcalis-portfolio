d852fa0832a4b0108e2d1566e87d1b2db8ddf424
fatihcaliss <fthcls@gmail.com>
2024-01-09 17:04:37 +0300

    add projects section

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -7,24 +7,45 @@ const Projects = () => {
       className="flex flex-col items-center justify-center py-20"
       id="projects"
     >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 pt-20 pb-10">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="cursive text-[20px] font-medium text-center text-gray-300 pb-10">
+        It is recommended to review the github profile for more project.
+      </div>
+      <div className="h-full w-full flex gap-10 px-10 justify-center  flex-wrap">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/fazlawebsite.png"
+          title="B2B E-Commerce Website"
+          description="As a Frontend Developer, I played a pivotal role in the creation of a high-performance commercial website using
+          Next.js, Material-UI, and Tanstack Query."
+          url="https://market.fazla.com/en"
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/esperanzawebsite.png"
+          title="Esperanza Grup"
+          description="In this project, I developed a single-page application that showcases the cafes owned by the client and the
+          services they offer. With a focus on user-friendliness and engaging design, I worked diligently to create a
+          customer-oriented solution"
+          url="https://www.esperanzagrup.com.tr/"
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/movieappwebsite.png"
+          title="Movie App"
+          description="User can watch movies trailers and get info about movies after login.I use firebase for auth."
+          url="https://movie-app-firebase-mu.vercel.app/"
+        />
+        <ProjectCard
+          src="/githubstatisticwebsite.png"
+          title="GitHub User Charts"
+          description="User can search a github user and can see the info charts abaout user. Try it now!"
+          url="https://github-user-statistics.vercel.app/home"
+        />
+        <ProjectCard
+          src="/followerdetectorwebsite.png"
+          title="GitHub follower detector"
+          description="You can list of github users which you are following but they don't follow you.Try it now!"
+          url="https://github-followers-app.vercel.app/"
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,28 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
   src: string;
   title: string;
   description: string;
+  url: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, url }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
+    <Link href={url} className="z-40 block" target="_blank">
+      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-64 h-[450px]">
+        <Image
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
 
-      <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+        <div className="relative p-4">
+          <h1 className="text-2xl font-semibold text-white">{title}</h1>
+          <p className="mt-2 text-gray-300">{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
